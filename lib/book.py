e75592439c598768dda714d29c0828f59edbfb3d
Iankiburio <ian.kiburio@student.moringaschool.com>
2023-10-17 15:59:51 +0300

    commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
+
 class Book:
+    def __init__(self, title, page_count):
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+        self.title = title
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
     pass
     
         
--- lib/testing/book_test.py
+++ lib/testing/book_test.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
 from book import Book
-
 import io
 import sys
+import pytest
 
 class TestBook:
     '''Book in book.py'''
@@ -11,17 +11,15 @@ class TestBook:
     def test_has_title_and_page_count(self):
         '''has the title and page_count passed into __init__, and values can be set to new instance.'''
         book = Book("And Then There Were None", 272)
-        assert(book.page_count == 272)
-        assert(book.title == "And Then There Were None")
+        assert book.page_count == 272
+        assert book.title == "And Then There Were None"
 
     def test_requires_int_page_count(self):
         '''prints "page_count must be an integer" if page_count is not an integer.'''
         book = Book("And Then There Were None", 272)
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        book.page_count = "not an integer"
-        sys.stdout = sys.__stdout__
-        assert captured_out.getvalue() == "page_count must be an integer\n"
+        with pytest.raises(TypeError) as e:
+            book.page_count = ("not an integer")
+        assert str(e.value) == "page_count must be an integer"
 
     def test_can_turn_page(self):
         '''outputs "Flipping the page...wow, you read fast!" when method turn_page() is called'''
@@ -30,4 +28,4 @@ class TestBook:
         sys.stdout = captured_out
         book.turn_page()
         sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "Flipping the page...wow, you read fast!\n")
+        assert captured_out.getvalue() == "Flipping the page...wow, you read fast!\n"
